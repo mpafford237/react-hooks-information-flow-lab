25169079a656ab86daa653612c1f411700f1ca94
masonPafford <mdpafford2015@gmail.com>
2024-08-08 12:22:12 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
+
+//Create a `Header` component by refactoring the `<header>` element out of the
+//`App` component. Clicking on the `<button>` inside of the `Header` component
+//should still toggle dark mode on and off.
+
+//You will need to pass a _callback function_ as a prop called `onDarkModeClick`
+//to the `Header` component in order for the test to pass.
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -11,12 +19,7 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      <Header onDarkModeClick={onDarkModeClick} isDarkMode={isDarkMode} />
       <ShoppingList items={itemData} />
     </div>
   );
